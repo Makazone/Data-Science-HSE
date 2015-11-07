5771443e2d603b0ddcb18d77baaebfd590cfc50a
Alexey Mileev <alemileev@gmail.com>
2015-11-07 18:07:36 +0300

    task 2 code

--- src/main.R
+++ src/main.R
@@ -1,5 +1,5 @@
 data = read.csv("data/data.csv", sep = ",")
-# We select H (Magnitude) as our ma
+# We select H (Magnitude) as our mama
 selectedFeature = data$H
 
 # Build histogram 
@@ -18,7 +18,31 @@ Mode = function(x) {
 }
 Mode(selectedFeature)
 
-# and the rest
-
+# median and mean
 median(selectedFeature)
 mean(selectedFeature)
+
+# Confidence interval
+n <- nrow(data)
+m <- mean(selectedFeature)
+s <- sd(selectedFeature)
+conf.int <- c(m - 1.965 * s / sqrt(n), m + 1.965 * s / sqrt(n))
+cat(sprintf("CI = (%f; %f)", conf.int[1], conf.int[2]))
+
+# Bootstrapping
+# pivotal
+n <- 5000
+means <- c()
+for (i in 1:n) {
+  means[i] <- mean(sample(selectedFeature, nrow(data), replace = TRUE))
+}
+hist(means) # like a normal, bitch! to-do-do-do-to-do-do...
+m <- mean(means)
+s <- sd(means)
+pivotal.conf.int <- c(m - 1.965 * s / sqrt(n), m + 1.965 * s / sqrt(n))
+cat(sprintf("PCI = (%f; %f)", pivotal.conf.int[1], pivotal.conf.int[2]))
+
+# nonpivotal
+sorted.means  <- sort(means)
+nonpivotal.conf.int <- c(sorted.means[0.025 * n], sorted.means[n - 0.025 * n])
+cat(sprintf("NPCI = (%f; %f)", nonpivotal.conf.int[1], nonpivotal.conf.int[2]))
