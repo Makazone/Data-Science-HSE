c31fca12e66f4845d49a43c4630b40cc7f7cbe7c
Stetsenko Makar <makazone@gmail.com>
2015-11-09 17:56:01 +0300

    HW 3, tasks 1, 2 and 3 completed

--- src/task3.R
+++ src/task3.R
@@ -1,1 +1,19 @@
 data = read.csv("data/data.csv", sep = ",")
+attach(data) # helps to avoid writing data$Q everytime
+
+# Scatter plot of all features, except PHA and discovery_date
+pairs(~H+MOID+q+Q+period, data=data, main="Scatterplot Matrix")
+
+# Looks like we have a strong correlation! Ya!
+plot(Q, period, main="Scatterplot of Q vs period",  xlab="Q", ylab="period", pch=19)
+
+# Now onto Y = aX + b
+regression = lm(period~Q)$coefficients
+cat(sprintf("period = %f Q + %f\n", regression[2], regression[1]))
+
+# Add bright red line to our scatter plot
+abline(regression, col="red") # regression line (y~x) 
+
+# How about mean absolute error?
+MAE = sum(abs(lm(period~Q)$residuals))/70
+cat(sprintf("MAE = %f\n", MAE))
