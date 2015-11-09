0ba138356ce0e04cc23f9ba7a87992e57ae84fc0
Stetsenko Makar <makazone@gmail.com>
2015-11-09 18:41:04 +0300

    Task 3.4 90% done

--- src/task3.R
+++ src/task3.R
@@ -8,6 +8,7 @@ pairs(~H+MOID+q+Q+period, data=data, main="Scatterplot Matrix")
 plot(Q, period, main="Scatterplot of Q vs period",  xlab="Q", ylab="period", pch=19)
 
 # Now onto Y = aX + b
+model = lm(period~Q)
 regression = lm(period~Q)$coefficients
 cat(sprintf("period = %f Q + %f\n", regression[2], regression[1]))
 
@@ -17,3 +18,10 @@ abline(regression, col="red") # regression line (y~x)
 # How about mean absolute error?
 MAE = sum(abs(lm(period~Q)$residuals))/70
 cat(sprintf("MAE = %f\n", MAE))
+
+# Coefficient of Determination
+r = summary(model)$r.squared
+cat(sprintf("Coefficient of Determination = %f\n", r))
+
+# And finaly correlation of period and Q
+cat(sprintf("Cov(period, Q) = %f\n", cor(Q, period)))
