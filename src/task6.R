178d88d033f7ba86a5c5dc77cca4b9cb24fcfe62
Stetsenko Makar <makazone@gmail.com>
2015-12-16 00:16:26 +0300

    Implement Anomalous pattern method

--- src/task6.R
+++ src/task6.R
@@ -53,3 +53,60 @@ ggplot(data, aes(Area, Groove_g)) +
   labs(colour = "Cluster ID") +
   geom_point(colour="grey90", size = 1.5) +
   ggtitle("Clustering Results")
+
+# Anomalous pattern method #
+data = subset(full_data, select = -Type)
+
+# @param data - data.frame
+# @return (S_k, c_k) the anamalous cluster list and its center
+AnomalousPattern = function(data) {
+  # 1. Pre-processing: Specify a reference point a = (a1, ..., aV ) (when in doubt, take a to be the data grand mean) 
+  a = as.data.frame(lapply(data, mean))
+  # and standardize the original data table by shifting the origin to a = (a1 , ..., aV ).      
+  # Do some shit 
+  
+  # Initial setting: Put a tentative centroid, c, as the entity farthest away from the origin, 0.
+  c = c()
+  minDist = Inf
+  for (i in 1:nrow(data)) {
+    d = dist(rbind(data[i,], a))
+    if (minDist > d) {
+      c = data[i,]
+      minDist = d
+    }
+  }
+  
+  c.init = c(c)
+  
+  c.prev = c()
+  for (i in 1:20) {
+    S = c()
+    # Cluster update: Determine cluster list S around c against the only other “centroid” 0
+    # so that entity yi is assigned to S if d(yi , c) < d(yi , 0).
+    for (i in 1:nrow(data)) {
+      d.c    = dist(rbind(data[i,], c))  
+      d.zero = dist(rbind(data[i,], a))
+      if (d.c < d.zero) {
+        S <- c(S, i)
+      } 
+    }
+    
+    # c.prev = c
+    c = as.data.frame(lapply(data[S, ], mean))
+  }
+  
+  return(list(S, c))
+}
+
+result = AnomalousPattern(data)
+S = result[1]
+c = result[2]
+
+
+
+
+
+
+
+
+
