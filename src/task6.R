0bca6ea9d3729661acc21ace9d39e9ee962f890d
Stetsenko Makar <makazone@gmail.com>
2015-12-15 18:04:35 +0300

    Init last HW

---
+++ src/task6.R
@@ -0,0 +1,49 @@
+library(e1071)
+library(class)
+
+# ─────────────NYA────────────────────────
+# ───▐▀▄───────▄▀▌───▄▄▄▄▄▄▄─────────────
+# ───▌▒▒▀▄▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄──────────
+# ──▐▒▒▒▒▀▒▀▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▀▄────────
+# ──▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▒▒▒▒▒▒▒▒▒▒▒▒▀▄──────
+# ▀█▒▒▒█▌▒▒█▒▒▐█▒▒▒▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌─────
+# ▀▌▒▒▒▒▒▒▀▒▀▒▒▒▒▒▒▀▀▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐───▄▄
+# ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌▄█▒█
+# ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▒█▀─
+# ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒█▀───
+# ▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌────
+# ─▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐─────
+# ─▐▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▌─────
+# ──▌▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▐──────
+# ──▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌──────
+# ────▀▄▄▀▀▀▀▀▄▄▀▀▀▀▀▀▀▄▄▀▀▀▀▀▄▄▀────────
+
+full_data = read.csv("data/sasha_data.csv", sep = ",")
+attach(full_data)
+
+full_data = full_data[which(Leuc < 1000), ]
+
+includedVars = c("Age", "Leuc", "Leber", "Milz") 
+data = full_data[includedVars]
+
+normalize = function(x) {
+  y = (x - min(x))/(max(x) - min(x))
+  y
+}
+
+data = as.data.frame(lapply(data, normalize))
+
+# -------------------- #
+
+includedVars = c("Q", "q", "MOID", "H", "period") 
+data = read.csv("data/data.csv", sep = ",")
+data = data[includedVars]
+data = as.data.frame(lapply(data, normalize))
+
+km.out=kmeans(data, 2,nstart=20)
+km.out$cluster
+
+plot(data, col=(km.out$cluster+1), main="K-Means Clustering Results with K=2", pch=20, cex=2)
+
+
+
