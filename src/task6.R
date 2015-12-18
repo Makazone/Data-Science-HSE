22c02ebeef090974739a49ef560a4bbaf8ed0633
Stetsenko Makar <makazone@gmail.com>
2015-12-18 18:50:30 +0300

    Fix ik Means

--- src/task6.R
+++ src/task6.R
@@ -2,6 +2,19 @@ library(e1071)
 library(class)
 library(ggplot2)
 
+# Norm functions
+normalize = function(x) {
+  y = (x - min(x))/(max(x) - min(x))
+  y
+}
+
+# Norm function to bring data to (0,0)
+# Used in iK-means and Anomalous Pattern
+normalizeByMean = function(x) {
+  y = (x - mean(x))/(max(x) - min(x))
+  y
+}
+
 # ─────────────NYA────────────────────────
 # ───▐▀▄───────▄▀▌───▄▄▄▄▄▄▄─────────────
 # ───▌▒▒▀▄▄▄▄▄▀▒▒▐▄▀▀▒██▒██▒▀▀▄──────────
@@ -19,6 +32,8 @@ library(ggplot2)
 # ──▐▄▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▄▌──────
 # ────▀▄▄▀▀▀▀▀▄▄▀▀▀▀▀▀▀▄▄▀▀▀▀▀▄▄▀────────
 
+# Prep data
+
 full_data = read.table("data/seeds_dataset.txt")
 # 1. area A, 
 # 2. perimeter P, 
@@ -33,16 +48,10 @@ colnames(full_data) = c("Area", "Perimeter", "Compactness", "Length_k", "Width_k
 # Since Compactness can be computed from Area and Perimeter, we exclude it 
 full_data = subset(full_data, select = -Compactness)
 
+############### SIMPLE K-MEANS ###############
+
 data = subset(full_data, select = -Type)
-
-normalize = function(x) {
-  y = (x - min(x))/(max(x) - min(x))
-  y
-}
-
 data = as.data.frame(lapply(data, normalize))
-
-# -------------------- #
 km.out=kmeans(data, 4, nstart=20)
 #km.out$cluster
 
@@ -54,16 +63,21 @@ ggplot(data, aes(Area, Groove_g)) +
   geom_point(colour="grey90", size = 1.5) +
   ggtitle("Clustering Results")
 
-# Anomalous pattern method #
-data = subset(full_data, select = -Type)
+
+
+
+
+############### ANOMALOUS PATTERN METHOD ###############
 
 # @param data - data.frame
 # @return (S_k, c_k) the anamalous cluster list and its center
-AnomalousPattern = function(data) {
-  # 1. Pre-processing: Specify a reference point a = (a1, ..., aV ) (when in doubt, take a to be the data grand mean) 
-  a = as.data.frame(lapply(data, mean))
-  # and standardize the original data table by shifting the origin to a = (a1 , ..., aV ).      
-  # __TODO__ Do some shit 
+AnomalousPattern = function(data, normalized = TRUE) {
+  if (!normalized) {
+    data = as.data.frame(lapply(data, normalizeByMean))
+  }
+  
+  # Take 0 point as a fixed center
+  a = rep(0,ncol(data)) 
   
   # Initial setting: Put a tentative centroid, c, as the entity farthest away from the origin, 0.
   c = c()
@@ -76,10 +90,12 @@ AnomalousPattern = function(data) {
     }
   }
   
+  # Furthest point 
+  # Used for DEBUG ONLY
   c.init = c(c)
   
   c.prev = c()
-  for (i in 1:20) {
+  for (i in 1:50) {
     S = c()
     # Cluster update: Determine cluster list S around c against the only other “centroid” 0
     # so that entity yi is assigned to S if d(yi , c) < d(yi , 0).
@@ -98,24 +114,51 @@ AnomalousPattern = function(data) {
   return(list(S, c))
 }
 
-result = AnomalousPattern(data)
+################## TEST Anomalous pattern ##################
+data = subset(full_data, select = -Type)
+data = as.data.frame(lapply(data, normalizeByMean))
+result = AnomalousPattern(data[, c("Area", "Groove_g")])
 S = result[[1]]
 c = rbind(result[[2]])
+data[,"Cluster"] = 1
+data[S, "Cluster"] = 2
+
+ggplot(data, aes(Area, Groove_g)) + 
+  aes(shape = factor(full_data$Type, labels = c("Kama", "Rosa", "Canadian"))) +
+  labs(shape = "Plant type") +
+  geom_point(aes(colour = factor(data[,"Cluster"])), size = 4) +
+  labs(colour = "Cluster ID") +
+  geom_point(colour="grey90", size = 1.5) +
+  ggtitle("Clustering Results")
+################## TEST Anomalous pattern ##################
+
+
+################## iK-means ##################
 
 # ------------------------------- #
 # iK-means #
 data = subset(full_data, select = -Type)
+data = as.data.frame(lapply(data, normalizeByMean))
+
+DataScatter = function(cluster) {
+  return(Reduce("+", cluster^2))
+}
 
 # Setting: Preprocess and standardize the dataset. Take t as the threshold of resolution. 
 # Put k = 1 and Ik = I, the original entity set.
 I = data
 k = 1
-t = 10
+K_max = 50
+minClusterSize = 5
+
+S = list()
+
+initDataScatter = DataScatter(data)
 
 while (TRUE) {
   # Anomalous pattern: Apply AP to Ik to find k-th anomalous pattern Sk and its centroid ck.
   anamResults = AnomalousPattern(I)
-  S = anamResults[[1]]
+  S[[length(S)+1]] = anamResults[[1]]
   
   if (k == 1) {
     c = rbind(anamResults[[2]])
@@ -125,31 +168,66 @@ while (TRUE) {
   
   # Test: If Stop-condition does not hold: 
   #   - All of I has been clustered, S.k = I.k;
-  #   - The total contribution of the first k clusters to the data scatter T has
-  #     reached a pre-specified threshold such as 50 %.
-  #   - Contribution of Sk is too small; for example, it is comparable with the
-  #     average contribution of a single entity, T/N, where T is the data scatter.
-  #   - Number of clusters k has reached its pre-specified value K.
-  # remove S.k from I, make k +=1 and I = I − S.k, after which step 1 is executed again. 
-  # Otherwise proceed 
-  
-  if (identical(data[S, ], data) | length(S) < t | k > 3) {
+  if (identical(data[S[[length(S)]], ], data)) {
+    length(S[[length(S)]])
+    print("All of I has been clustered")
     break
   }
   
-  I = as.data.frame(lapply(I, "[", -S))
+  #   - The total contribution of the first k clusters to the data scatter T has
+  #     reached a pre-specified threshold such as 50 %.
+  totalContribution = 0
+  for (i in 1:length(S)) {
+    totalContribution = totalContribution + DataScatter(data[S[[i]],])
+  }
+  if (totalContribution / initDataScatter > 0.8) {
+    print("Data scatter of first k clusters is greater 50")
+    break
+  }
+  
+  print("1")
+  
+  #   - Contribution of Sk is too small; for example, it is comparable with the
+  #     average contribution of a single entity, T/N, where T is the data scatter.
+  averageContribution = initDataScatter / nrow(data)
+  if (abs(DataScatter(S[[length(S)]]) - averageContribution) < 0.1) {
+    print("Small contribution")
+    break
+  }
+  print("2")
+  #   - Number of clusters k has reached its pre-specified value K.
+  # remove S.k from I, make k +=1 and I = I − S.k, after which step 1 is executed again. 
+  if (k > K_max) {
+    print("k max reached")
+    break
+  }
+  
+  print("3")
+  # Otherwise proceed 
+  
+  #I = as.data.frame(lapply(I, "[", -S[[length(S)]]))
+  I = I[-S[[length(S)]], ]
   k = k + 1
   
   if (length(I) == 0) {
     break
   }
+  print(k)
 }
 
 # Discarding small clusters: 
 # Remove all of the found clusters containing t entities or less. 
 # Denote the number of remaining clusters by K and re-label them so that their centroids are c1, c2, ..., cK
+smallClusterIndecies = c()
+for (i in 1:length(S)) {
+  if (length(S[[i]]) < minClusterSize) {
+    smallClusterIndecies = c(smallClusterIndecies, i) 
+  }
+}
 
-# __TODO__
+if (!is.null(smallClusterIndecies)) {
+  c = c[-smallClusterIndecies, ]
+}
 
 # Do K-Means using c1, c2, ..., cK as initial seeds.
 km.out = kmeans(data, c)
